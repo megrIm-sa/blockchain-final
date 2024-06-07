76536f6a2f2a46cd7a6168cb4593f76af81ecbe4
Shvetkoy Anton <121558506+megrIm-sa@users.noreply.github.com>
2024-06-07 17:14:33 +0500

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
     solidity: "0.8.0",
     networks: {
         sepolia: {
-            url: "https://sepolia.infura.io/v3/804b0813948e4580b599cd173b24a467",
+            url: "https://sepolia.infura.io/v3/7233f80b5baf477ca670c0b1658700dc",
             accounts: ["e449d3eb89bb22244ecf13b420fb98d60725050bb4d819a041b066569f1ebc06"],
         },
         goerli: {
